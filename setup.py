6be3556a574491d8f862f8be0597ff91167e3d54
Michał Mordawski <michal.mordawski@amartus.com>
2021-06-14 16:23:59 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='cloudify-breakpoint-plugin',
+    version='1.0.0',
+    author='Michal Mordawski',
+    author_email='Michal.Mordawski@cloudify.co',
+    license='LICENSE',
+    zip_safe=False,
+    packages=find_packages(exclude=['tests*']),
+    install_requires=['cloudify-common>=5.0.5'],
+)
